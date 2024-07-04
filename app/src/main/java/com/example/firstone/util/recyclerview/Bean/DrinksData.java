0b1bcfc6393cff4b81ebe808bfa6d73587a6cdb9
yukiwwwang <yukiwwwang@tencent.com>
2024-07-04 10:34:30 +0800

    update and WebView

--- app/src/main/java/com/example/firstone/util/recyclerview/Bean/DrinksData.java
+++ app/src/main/java/com/example/firstone/util/recyclerview/Bean/DrinksData.java
@@ -1,6 +1,24 @@
 package com.example.firstone.util.recyclerview.Bean;
 
+import com.example.firstone.R;
+
 public class DrinksData {
+    public static int[] icon = {
+            R.mipmap.drinks_kola,
+            R.mipmap.drinks_xuebi,
+            R.mipmap.drinks_fenda,
+            R.mipmap.drinks_milk,
+            R.mipmap.drinks_yogurt,
+            R.mipmap.drinks_ad,
+            R.mipmap.drinks_ice_red_tea,
+            R.mipmap.drinks_molilvcha,
+            R.mipmap.drinks_moliqingcha,
+            R.mipmap.drinks_red_tea,
+            R.mipmap.drinks_green_tea,
+            R.mipmap.drinks_puer,
+            R.mipmap.drinks_xuanmi,
+            R.mipmap.drinks_orange,
+    };
     public static String[] title = {
             "可乐",
             "雪碧",
@@ -13,20 +31,20 @@ public class DrinksData {
             "茉莉清茶",
             "红茶",
             "绿茶",
-            "普洱茶",
+            "青柑普洱茶",
             "玄米茶",
-            "橘皮茶",
             "橙汁",
-            "桃汁",
-            "苹果汁",
-            "石榴汁",
-            "红枣茶",
-            "蜂蜜水",
-            "葡萄汁",
-            "柚子汁",
-            "青柠汁",
-            "西瓜汁",
-            "菠萝汁",
-            "西柚汁",
-            "乌龙茶",};
+//            "桃汁",
+//            "苹果汁",
+//            "石榴汁",
+//            "红枣茶",
+//            "蜂蜜水",
+//            "葡萄汁",
+//            "柚子汁",
+//            "青柠汁",
+//            "西瓜汁",
+//            "菠萝汁",
+//            "西柚汁",
+//            "乌龙茶",
+    };
 }
--- app/src/main/java/com/example/firstone/util/schedule/fragment/WorkFragment.java
+++ app/src/main/java/com/example/firstone/util/schedule/fragment/WorkFragment.java
@@ -1,20 +1,48 @@
 package com.example.firstone.util.schedule.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.firstone.R;
+import com.example.firstone.util.webview.WebViewActivity;
+import com.example.firstone.util.webview.WebViewActivity2;
 
 public class WorkFragment extends Fragment {
+
+    private Button mTestBtn;
+    private Button mTestBtn2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_work, container, false);
+        View view = inflater.inflate(R.layout.fragment_work, container, false);
+
+        mTestBtn = view.findViewById(R.id.test_btn);
+        mTestBtn2 = view.findViewById(R.id.test_btn2);
+        mTestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mTestBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), WebViewActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
+
 }
